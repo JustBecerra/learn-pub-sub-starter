14cc5278bed8a653a310b161973463d6b7fc240c
JustBecerra <justj.becerra@gmail.com>
2026-04-17 13:35:41 -0300

    add main config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to RabbitMQ")
+	// create a channel to listen for signals
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	received := <-sigCh
+	fmt.Println("Received signal:", received.String())
 }
