7b3709003a6195a6be00e3e88ce423268227e901
JustBecerra <justj.becerra@gmail.com>
2026-04-28 17:02:16 -0300

    add handler to move and notice to other players when moving

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -19,6 +19,13 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 	}
 }
 
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(move gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+		gs.HandleMove(move)
+	}
+}
+
 func main() {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
@@ -39,8 +46,14 @@ func main() {
 
 	err = pubsub.SubscribeJSON(conn, routing.ExchangePerilDirect, routing.PauseKey+"."+username, routing.PauseKey, pubsub.SimpleQueueTypeTransient, handlerPause(gs))
 	if err != nil {
-		log.Fatalf("Failed to declare and bind queue: %v", err)
+		log.Fatalf("Failed to subscribe to pause: %v", err)
 	}
+
+	err = pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, routing.ArmyMovesPrefix+"."+username, routing.ArmyMovesPrefix+".*", pubsub.SimpleQueueTypeTransient, handlerMove(gs))
+	if err != nil {
+		log.Fatalf("Failed to subscribe to army moves: %v", err)
+	}
+	log.Printf("Subscribed to %s", routing.ArmyMovesPrefix+".*")
 
 loop:
 	for {
@@ -54,7 +67,16 @@ loop:
 			gs.CommandSpawn(words)
 			fmt.Println("spawned a unit")
 		case "move":
-			gs.CommandMove(words)
+			move, err := gs.CommandMove(words)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			err = pubsub.PublishJSON(ch, routing.ExchangePerilTopic, routing.ArmyMovesPrefix+"."+username, move)
+			if err != nil {
+				fmt.Printf("error publishing move: %v\n", err)
+			}
+			fmt.Println("move was published")
 		case "status":
 			gs.CommandStatus()
 		case "spam":
